afea7f9b901bd61c9893bc1306c6e3c0ab4eec12
ASHISH <ashishsontakke12@gmail.com>
2022-02-19 09:42:28 +0530

    updated starter code

--- app/index.js
+++ app/index.js
@@ -1,29 +1,8 @@
 import Web3 from "web3";
-import MyCoin from "../build/contracts/MyCoin.json";
 
 const App = {
-  web3: null,
-  account: null,
-  meta: null,
-
   start: async function () {
-    const { web3 } = this;
-
-    try {
-      // get contract instance
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = ColorNFTArtifact.networks[networkId];
-      this.meta = new web3.eth.Contract(
-        ColorNFTArtifact.abi,
-        deployedNetwork.address
-      );
-      console.log(this.meta);
-      // get accounts
-      const accounts = await web3.eth.getAccounts();
-      this.account = accounts[0];
-    } catch (error) {
-      console.error(`Could not connect to contract or chain. ${error}`);
-    }
+    console.log("app started");
   },
 };
 
@@ -31,18 +10,10 @@ window.App = App;
 
 window.addEventListener("load", function () {
   if (window.ethereum) {
-    // use MetaMask's provider
     App.web3 = new Web3(window.ethereum);
-    window.ethereum.enable(); // get permission to access accounts
+    window.ethereum.enable();
   } else {
-    console.warn(
-      "No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live"
-    );
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    App.web3 = new Web3(
-      new Web3.providers.HttpProvider("http://127.0.0.1:8545")
-    );
+    console.warn("No web3 detected.");
   }
-
   App.start();
 });
